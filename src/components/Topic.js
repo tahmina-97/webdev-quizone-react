23b5d3c0659a754a3f75058ed20c56ad79847d9b
Tahmina Ria <tahminariya459@gmail.com>
2022-10-11 23:30:35 +0600

    topics display done

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 const Topic = ({ topic }) => {
-    const { id, name } = topic;
-    return (
-        <div>
-            <h1> some quizes {name} </h1>
+    const { id, name, logo, total } = topic;
 
+    return (
+        <div className="max-w-xs rounded-md shadow-md p-3 bg-slate-100">
+            <img src={logo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+            <div className="flex flex-col justify-between p-2 space-y-4">
+                <div className="space-y-3 flex justify-between items-center">
+                    <h2 className="text-3xl font-semibold tracking-wide">{name}</h2>
+                    <p className='font-semibold'>Quiz: <span className=' font-bold text-gray-800 '>{total}</span></p>
+                </div>
+                <button type="button" className="bg-indigo-600 text-white font-semibold flex justify-between items-center w-full p-3 tracking-wide rounded-md">
+                    <div>Start Quiz</div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                    </svg>
+
+
+
+
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -6,14 +6,16 @@ const Topics = () => {
     const topics = useContext(TopicsContext);
 
     return (
-        <div>
-            <h1 className='text-center'>Quiz on Fundamental Topics</h1>
-            {
-                topics.map(topic => <Topic
-                    key={topic.id}
-                    topic={topic}
-                ></Topic>)
-            }
+        <div className='mx-10 md:mx-20'>
+            <h1 className='text-center text-5xl font-bold text-gray-700 p-4 mb-14'>Quiz on Fundamental Topics</h1>
+            <div className='grid md:grid-cols-2 gap-8 justify-items-center'>
+                {
+                    topics.map(topic => <Topic
+                        key={topic.id}
+                        topic={topic}
+                    ></Topic>)
+                }
+            </div>
 
         </div>
     );
