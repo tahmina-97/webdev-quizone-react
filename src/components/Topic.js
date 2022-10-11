55f40d5d99e7230ff717c1128a367145808dd67a
Tahmina Ria <tahminariya459@gmail.com>
2022-10-11 23:36:45 +0600

    heroIcon

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ArrowLongRightIcon } from '@heroicons/react/24/solid'
 
 const Topic = ({ topic }) => {
     const { id, name, logo, total } = topic;
@@ -9,17 +10,11 @@ const Topic = ({ topic }) => {
             <div className="flex flex-col justify-between p-2 space-y-4">
                 <div className="space-y-3 flex justify-between items-center">
                     <h2 className="text-3xl font-semibold tracking-wide">{name}</h2>
-                    <p className='font-semibold'>Quiz: <span className=' font-bold text-gray-800 '>{total}</span></p>
+                    <p className='font-semibold'>Quiz : <span className=' font-bold text-gray-800 '>{total}</span></p>
                 </div>
                 <button type="button" className="bg-indigo-600 text-white font-semibold flex justify-between items-center w-full p-3 tracking-wide rounded-md">
                     <div>Start Quiz</div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                    </svg>
-
-
-
-
+                    <ArrowLongRightIcon className='h-8 w-8'></ArrowLongRightIcon>
                 </button>
             </div>
         </div>
